0e81594ea88ea06d5620ddb0d0a05476a1cf2d8f
Peng <nj.pengzhang@gmail.com>
2012-10-30 14:03:33 -0700

    Gauge

--- ledgauge.js
+++ ledgauge.js
@@ -3,129 +3,127 @@
  * @author pzhang
  */
 
- //frame
-var wrapper = document.getElementById('gauge');
-var width = parseInt(window.getComputedStyle(wrapper).width);
-var height = parseInt(window.getComputedStyle(wrapper).height);
-console.log(width, height);
-var paper = Raphael('gauge', width, height);
-var frame = paper.rect(60, 30, 50, 350-30).attr({ 	
-	stroke: '#cccccc',
-	'stroke-width': .1,
-	fill: "#000",
-	'fill-opacity': .25
-}); 
-frame.toBack();
-var frame_height = frame.attr("height");
-var frame_width = frame.attr("width");
-var startX = frame.attr("x");
-var startY = frame.attr("y") + frame_height;
+Raphael.fn.ledGauge = function(args) {	
+	var paper = this, 
+		width = parseInt(window.getComputedStyle(args.div).width),
+		height = parseInt(window.getComputedStyle(args.div).height),
+		margin_left = (typeof args.margin.left == 'undefined') ? 
+				30 : parseInt(args.margin.left),
+		margin_right = (typeof args.margin.right == 'undefined') ? 
+				20 : parseInt(args.margin.right),
+		margin_top = (typeof args.margin.top == 'undefined') ?
+				40 : parseInt(args.margin.top),
+		margin_bot = (typeof args.margin.bottom == 'undefined') ?
+				30 : parseInt(args.margin.bottom);
+	
+	var frame = paper.rect(
+			margin_left, 
+			margin_top, 
+			width-(margin_left+margin_right), 
+			height-(margin_top+margin_bot))
+		.attr({ 	
+			stroke: '#cccccc',
+			'stroke-width': .1,
+			fill: "#000",
+			'fill-opacity': .4
+		}),
+		frame_height = frame.attr("height"),
+		frame_width = frame.attr("width"),
+		startX = frame.attr("x"),
+		startY = frame.attr("y") + frame_height;
+	frame.toBack();
 
-var mark_1 = paper.path("M"+startX+","+startY+"h"+(-10));
-var mark_1_value = paper.text(startX-10, startY, "0").attr({
-	'text-anchor': 'end',
-	'font-size': 14
-});
-var mark_2 = paper.path("M"+startX+","+(startY-frame.attr("height")/2)+"h-10");
-var mark_2_value = paper.text(startX-10, (startY-frame.attr("height")/2), "50")
-						.attr({
-							'text-anchor': 'end',
-							'font-size': 14
-						});
-var mark_3 = paper.path("M"+startX+","+(startY-frame.attr("height"))+"h-10");
-var mark_3_value = paper.text(startX-10, (startY-frame.attr("height")), "100")
-						.attr({
-							'text-anchor': 'end',
-							'font-size': 14
-						});
-//thresholds
-var thresholdValues = [46, 75, 100];
-var thresholdColors = ["#00ff00", "#f0f000", "#ff0000"];
-var thresholdSet = paper.set();
-for (var i = 0; i < 3; i += 1) {
-	var percentage = thresholdValues[i]/100;
-	console.log(startY-frame_height*percentage);
-	var t = paper.rect(
-		startX, 
-		startY-frame_height*percentage,
-		frame_width,
-		frame_height*percentage
-	).attr({
-		fill: thresholdColors[i]
+	var title = paper.text(
+			width/2, 
+			20, 
+			(typeof args.title == 'undefined')?'':args.title)
+		.attr({
+			'text-anchor': 'middle',
+			'font-size': 16,
+			'stroke-width': 3
+		});
+	var unit = ((typeof args.title == 'undefined') ? '':args.unit),
+		bot_number = paper.text(
+			width/2,
+			startY + 20,
+			args.min+unit
+		).attr({
+			'text-anchor': 'middle',
+			'font-size': 16,
+			'stroke-width': 3
+		});
+	
+	var mark_1 = paper.path("M"+startX+","+startY+"h"+(-6)),
+		mark_1_value = paper.text(startX-5, startY, args.min).attr({
+			'text-anchor': 'end',
+			'font-size': 14
+		}),
+		mark_2 = paper.path("M"+startX+","+(startY-frame.attr("height")/2)+"h-6"),
+		mark_2_value = paper.text(startX-6, 
+			(startY-frame.attr("height")/2), (args.min+args.max)/2)
+							.attr({
+								'text-anchor': 'end',
+								'font-size': 14
+							}),
+		mark_3 = paper.path("M"+startX+","+(startY-frame.attr("height"))+"h-6"),
+		mark_3_value = paper.text(startX-6, 
+			(startY-frame.attr("height")), args.min+args.max)
+							.attr({
+								'text-anchor': 'end',
+								'font-size': 14
+							});
+	//thresholds
+	var thresholdValues = args.thresholds.values,
+		thresholdColors = args.thresholds.colors,
+		thresholdSet = paper.set();
+	for (var i = 0; i < thresholdValues.length; i += 1) {
+		var percentage = thresholdValues[i]/100;
+		var t = paper.rect(
+			startX, 
+			startY-frame_height*percentage,
+			frame_width,
+			frame_height*percentage
+		).attr({
+			fill: thresholdColors[i]
+		});
+		thresholdSet.push(t);
+	}
+	thresholdSet.forEach(function(shape){
+		shape.toBack();
 	});
-	thresholdSet.push(t);
-}
-thresholdSet.forEach(function(shape){
-	shape.toBack();
-});
 
-var bar_number = 60; //changable
-var bar_height = frame_height/bar_number,
-	bar_width = frame_width,
-	initX = startX,
-	initY = startY,
-	isEven = false,
-	barSet = paper.set();
-for (var i = 0; i < bar_number; i += 1) {
-	initY = initY - bar_height;
-	var bar = paper.rect(
-		initX, 
-		initY, 
-		bar_width, 
-		(isEven) ? (bar_height-1) : (bar_height+1))
-				   .attr({
-				   	fill: (isEven) ? '#333' : '',
-				   	'stroke-width': .1,
-				   	stroke: '#000'
-				   });
-	barSet.push(bar);
-	isEven = !isEven;
-}
-
-paper.rect(frame.attr("x"), frame.attr("y"), frame.attr("width"), frame.attr("height"))
-
-//trick, show value.
-var value = 79.3,
-	bright_percentage = value/100,
-	dark_percentage = 1 - bright_percentage;
-
-var newHeight = dark_percentage*frame_height;
-console.log(newHeight, "=");
-frame.attr({
-	height: newHeight
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	var bar_number = 60,
+		bar_height = frame_height/bar_number,
+		bar_width = frame_width,
+		initX = startX,
+		initY = startY,
+		isEven = false,
+		barSet = paper.set();
+	for (var i = 0; i < bar_number; i += 1) {
+		initY = initY - bar_height;
+		var bar = paper.rect(
+			initX, 
+			initY, 
+			bar_width, 
+			(isEven) ? (bar_height-1) : (bar_height+1))
+					   .attr({
+					   	fill: (isEven) ? '#333' : '',
+					   	'stroke-width': .1,
+					   	stroke: '#000'
+					   });
+		barSet.push(bar);
+		isEven = !isEven;
+	}
+	this.setTo = function(value) {
+		var bright_percentage = value/(args.max-args.min),
+			dark_percentage = 1 - bright_percentage,
+			newHeight = dark_percentage*frame_height;
+		frame.animate({
+			height: newHeight
+		}, 600);		
+		bot_number.attr({
+			text: value + ((typeof args.title == 'undefined') ? '':args.unit)
+		});
+	};
+	return this;
+};
